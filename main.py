606df463e1f2468dbcdbb6c87d89ff945ff741ce
inbartz <inbartz@post.bgu.ac.il>
2021-04-13 22:29:08 +0300

    fill none values

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import pandas as pd
+from sklearn.experimental import enable_iterative_imputer
+from sklearn.impute import IterativeImputer
 
 # Variables
 Not_blood_related = ['ï»¿Patient ID', 'Patient age quantile',
@@ -23,7 +25,6 @@ def read_data(path):
     data = pd.read_csv(path + "dataset.csv", encoding='latin-1')
     return data
 
-
 def choose_features(data, features_to_drop, Required_features):
     # Droop all the features that are not related to blood
     data.drop(features_to_drop, axis='columns', inplace=True)
@@ -37,12 +38,10 @@ def choose_features(data, features_to_drop, Required_features):
     data.dropna(thresh=2, inplace=True)
     return data
 
-
 def get_target_values(data , target_col_name):
     target_values = data[target_col_name]
     target_values = target_values.replace({'negative': 0, 'positive': 1})
     return target_values.to_frame()
-
 
 def data_pre_process(data, features_to_drop, Required_features, target_col_name):
     clean_data = choose_features(data, features_to_drop, Required_features)
@@ -50,15 +49,33 @@ def data_pre_process(data, features_to_drop, Required_features, target_col_name)
     clean_data.drop(target_col_name, axis='columns', inplace=True)
     return clean_data, target_values
 
+def fill_none_values(data, initial_strategy):
+    new_data = pd.DataFrame(data)
+    #imputer = IterativeImputer(random_state=0,  initial_strategy=initial_strategy)
+    imputer = IterativeImputer(random_state=0)
+    imputer.fit(new_data)
+    data_without_null = imputer.transform(new_data)
+    df_data = pd.DataFrame(data_without_null, columns=data.columns)
+    return df_data
+
+
 
 if __name__ == '__main__':
     # read data from csv file
     all_data = read_data(path)
     # preform pre-process includes:
-    # remove features that are not related to blood
-    # remove feature with more then 95% none values
-    # remove rows with more then 2
-    # Change the target value to binary
+    #   remove features that are not related to blood
+    #   remove feature with more then 95% none values
+    #   remove rows with more then 2
+    #   Change the target value to binary
     X, Y = data_pre_process(all_data, Not_blood_related, Required_features, target_col_name)
     print("This is X - features and values: "+str(X))
     print("This is Y - target values 0=negative, 1=positive: " + str(Y))
+    # Need to check which strategy gives better result
+    # X_mean_insteadof_none = fill_none_values(X, 'mean')
+    # X_median_insteadof_none = fill_none_values(X, 'median')
+    # print("This is X with mean values - features and values: " + str(X_mean_insteadof_none))
+    # print("This is X with median values - features and values: " + str(X_median_insteadof_none))
+    X = fill_none_values(X, '')
+    print("This is X without none values - features and values: "+str(X))
+
